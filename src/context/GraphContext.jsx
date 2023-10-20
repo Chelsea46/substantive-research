824f07fda69bb0de08914cfbc661b5f3e511616b
Chelsea Roberts <Chelsea.J.Roberts@hotmail.com>
2023-10-20 12:33:36 +0100

    graph context and array created

---
+++ src/context/GraphContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useEffect, useState } from "react"
+import axios from "axios";
+
+export const GraphContext = createContext();
+
+function GraphContextProvider(props) {
+
+  const [apiData, setApiData] = useState({});
+  const [interactionsData, setInteractionsData] = useState([]);
+
+  async function fetchApiData() {
+    try {
+      const response = await axios.get('https://substantive.pythonanywhere.com/')
+      setApiData(response.data.interactions);
+    } catch (error) {
+      console.error('Error fetching API data: ', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchApiData();
+  }, []);
+
+  useEffect(() => {
+    if (Array.isArray(apiData) && apiData.length > 0) {
+      // Calculate the count of interactions for each sector_id
+      const interactionsCount = {};
+
+      apiData.forEach(interaction => {
+        const sectorId = interaction.sector_id;
+        interactionsCount[sectorId] = (interactionsCount[sectorId] || 0) + 1;
+      });
+
+      // Create a new array of objects with id, name, and count
+      const newInteractionsData = Object.keys(interactionsCount).map(sectorId => {
+        const count = interactionsCount[sectorId];
+        const sectorName = apiData.find(interaction => interaction.sector_id === sectorId).name;
+        const totalInteractions = apiData.length;
+        const percentage = (count / totalInteractions) * 100;
+        return {
+          id: sectorId,
+          name: sectorName,
+          count: percentage,
+        };
+      });
+
+      setInteractionsData(newInteractionsData);
+    }
+  }, [apiData]);
+
+    // values to pass to components
+  const value = {
+    interactionsData,
+  };
+
+  return (
+      <GraphContext.Provider value={value}>
+      {props.children}
+      </GraphContext.Provider>
+  );
+}
+
+export default GraphContextProvider;
